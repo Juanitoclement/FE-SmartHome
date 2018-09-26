78c2fbd79caba20047c07144241239e234410a1e
Steve Vinsensius Jo <steve.v.jo@gmail.com>
2018-09-26 10:47:05 +0700

    TV GUI

--- src/assets/jss/smart-home-react/components/cardIconCustomStyle.jsx
+++ src/assets/jss/smart-home-react/components/cardIconCustomStyle.jsx
@@ -6,16 +6,15 @@ import {
   primaryCardHeader,
   roseCardHeader
 } from "assets/jss/smart-home-react.jsx";
-const cardIconStyle = {
-  cardIcon: {
+const cardIconCustomStyle = {
+  cardIconCustom: {
     "&$warningCardHeader,&$successCardHeader,&$dangerCardHeader,&$infoCardHeader,&$primaryCardHeader,&$roseCardHeader": {
       borderRadius: "3px",
       backgroundColor: "#999",
       padding: "15px",
       marginTop: "-20px",
       marginRight: "15px",
-      float: "left",
-
+      float: "left"
     }
   },
   warningCardHeader,
@@ -26,4 +25,4 @@ const cardIconStyle = {
   roseCardHeader
 };
 
-export default cardIconStyle;
+export default cardIconCustomStyle;
