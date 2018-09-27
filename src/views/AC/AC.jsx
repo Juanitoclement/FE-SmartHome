2457609ed1bfaaf2366401f2e7e52df235a4f60e
Steve Vinsensius Jo <steve.v.jo@gmail.com>
2018-09-27 14:08:37 +0700

    TV Power + AC Timer

--- src/views/AC/AC.jsx
+++ src/views/AC/AC.jsx
@@ -38,60 +38,59 @@ import {
 
 import dashboardStyle from "assets/jss/smart-home-react/views/dashboardStyle.jsx";
 
-import Dropdown from "react-dropdown"
-import "react-dropdown/style.css"
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
 
-const options = [
-  'Bedroom', 'Livingroom', 'Kamar Pembantu'
-]
+const options = ["Bedroom", "Livingroom", "Kamar Pembantu"];
 
 const styles = {
   cardColorTest: {
     backgroundColor: "rgba(255,255,255,.62)"
   }
-}
+};
 
 class AC extends React.Component {
   state = {
     value: 0,
-    acstatus: 0,
-    power: "OFF"
+    acstatus: 0
   };
 
-  handleChange = (event, value) => {
-    this.setState({ value });
-  };
-
-  handleChangeIndex = index => {
-    this.setState({ value: index });
-  };
   showUI() {
     window.location = "/table";
   }
 
-  handleColor = (int) => {
-    if(this.state.acstatus === 0){
+  handleColor = int => {
+    if (this.state.acstatus === 0) {
       return "info";
-    }
-    else {
+    } else {
       return "rose";
     }
-  }
+  };
 
-  button = () =>{
-    if(this.state.acstatus === 0){
+  button = () => {
+    if (this.state.acstatus === 0) {
       this.setState({
-        acstatus: 1,
-        power: "ON"
+        acstatus: 1
+      });
+    } else {
+      this.setState({
+        acstatus: 0
       });
     }
-    else {
-      this.setState({
-        acstatus: 0,
-        power: "OFF"
-      });
-    }
+  };
+
+  handleTimer = () => {
+    this.setState({
+      h: this.state.number,
+      m: 0,
+      s: 0
+    });
   }
+  handleInput = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
 
   render() {
     const { classes } = this.props;
@@ -101,20 +100,20 @@ class AC extends React.Component {
           {/*Power Off button*/}
           <GridItem xs={12} sm={6} md={3} lg={12}>
             <Card>
-
               {/* Power Off / Main Card */}
               <CardHeader color="info" stats icon>
-                <CardIcon onClick={this.button} color={this.handleColor(this.state.acstatus)}>
+                <CardIcon
+                  onClick={this.button}
+                  color={this.handleColor(this.state.acstatus)}
+                >
                   <Icon>power_settings_new</Icon>
                   <p>{this.state.power}</p>
-
                 </CardIcon>
               </CardHeader>
 
               <CardBody>
                 {/* Seperate The 2 card inside */}
                 <GridContainer>
-
                   {/* Minus Temperature Button */}
                   <GridItem xs={6} sm={6} md={6} lg={6}>
                     <Card>
@@ -150,11 +149,35 @@ class AC extends React.Component {
                   {/* Dropdown Menu */}
                   <GridItem xs={12} sm={12} md={12} lg={12}>
                     <h3>Select AC:</h3>
-                    <Dropdown options={options} onChange={this._onSelect} value={options[0]} placeholder="TEsting123" />
+                    <Dropdown
+                      options={options}
+                      onChange={this._onSelect}
+                      value={options[0]}
+                      placeholder="TEsting123"
+                    />
+                  </GridItem>
+
+                  {/* Timer Menu */}
+                  <GridItem xs={12} sm={12} md={12} lg={12}>
+                    <h3>Timer</h3>
+                    <form>
+                      <input
+                        name="number"
+                        type="number"
+                        placeholder="Time in hours"
+                        onChange={event => this.handleInput(event)}
+                      />
+                      {/*<p>{this.state.number}</p>*/}
+                      <button type="button" onClick={this.handleTimer}>
+                        Submit
+                      </button>
+                      <br />
+                      h: {this.state.h} m: {this.state.m} s:{" "}
+                      {this.state.s}
+                    </form>
                   </GridItem>
                 </GridContainer>
               </CardBody>
-
             </Card>
           </GridItem>
         </GridContainer>
