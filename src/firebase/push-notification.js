210a4e4bd7c34c489e025cd2d371a4c22303ab90
Clement Juanito <xdigoxinx@gmail.com>
2018-10-20 17:52:57 +0700

    firebase

--- src/firebase/push-notification.js
+++ src/firebase/push-notification.js
@@ -1,16 +1,17 @@
 import firebase from "firebase";
+import store from "../redux/store/configureStore";
+import { notificationToken } from "../redux/actions/firebaseAction";
+import Button from "components/CustomButtons/Button.jsx";
+import Snackbar from "components/Snackbar/Snackbar.jsx";
+import dashboardStyle from "assets/jss/smart-home-react/views/dashboardStyle.jsx";
 
-export const initializeFirebase = () => {
-  console.log("sdasdasdadsadsa");
-
-  firebase.initializeApp({
-    apiKey: "AIzaSyAeM1YvCtKhi6xqXTUMsZ8HxC-fAgXutBU",
-    authDomain: "homie-6702b.firebaseapp.com",
-    databaseURL: "https://homie-6702b.firebaseio.com",
-    projectId: "homie-6702b",
-    storageBucket: "homie-6702b.appspot.com",
-    messagingSenderId: "654798426208"
-  });
+export const config = {
+  apiKey: "AIzaSyAeM1YvCtKhi6xqXTUMsZ8HxC-fAgXutBU",
+  authDomain: "homie-6702b.firebaseapp.com",
+  databaseURL: "https://homie-6702b.firebaseio.com",
+  projectId: "homie-6702b",
+  storageBucket: "homie-6702b.appspot.com",
+  messagingSenderId: "654798426208"
 };
 export const askForPermissionToReceiveNotifications = async () => {
   try {
@@ -23,14 +24,19 @@ export const askForPermissionToReceiveNotifications = async () => {
       .catch(function(err) {
         console.log("error", err);
       });
-    messaging.onMessage(function(payload) {
-      console.log("onMessage: ", payload.notification);
-    });
+    getNotif();
     const token = await messaging.getToken();
     console.log("token:", token);
-
+    store.store.dispatch(notificationToken(token));
     return token;
   } catch (error) {
     console.error(error);
   }
+};
+export const getNotif = async () => {
+  const messaging = firebase.messaging();
+  messaging.onMessage(function(payload) {
+    console.log("onMessage: ", payload.notification);
+    return <p>gelp</p>;
+  });
 };
